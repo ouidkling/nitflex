ee11ebbc14131a2d81b0d173cc6cea29059108ea
Temmie <75093246+ouidkling@users.noreply.github.com>
2024-03-03 22:41:49 +0100

    Message broker (#4)
    * Docker compose configuration
    
    * message broker (help me)
    
    * changed a bit of stuff

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NITFLEX
+
+## Run docker
+```bash
+docker compose --env-file <path to .env> up
+```
--- seeds/user.js
+++ seeds/user.js
@@ -11,8 +11,10 @@ exports.seed = function (knex) {
 					firstname: 'admin',
 					lastname: 'admin',
 					username: 'admin',
-					email: process.env.ADMIN_EMAIL,
-					password: Encrypt.rsaSha256(process.env.ADMIN_PASSWORD),
+					email: process.env.ADMIN_EMAIL || 'admin@example.com',
+					password: Encrypt.rsaSha256(
+						process.env.ADMIN_PASSWORD || 'admin'
+					),
 					scope: JSON.stringify(['admin', 'user']),
 				},
 				{
@@ -20,7 +22,7 @@ exports.seed = function (knex) {
 					lastname: 'Doe',
 					username: 'johndoe',
 					email: 'johndoe@example.com',
-					password: Encrypt.rsaSha256('p4ssw0rd'),
+					password: Encrypt.rsaSha256('password'),
 					scope: JSON.stringify(['user']),
 				},
 			]);
